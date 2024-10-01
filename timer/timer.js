900db8987fe661bc1b4c75e2f293dd1213586dc2
Trinigch <trinigch@gmail.com>
2024-10-01 15:10:48 -0500

    fix bug in timer

--- timer/timer.js
+++ timer/timer.js
@@ -1,20 +1,25 @@
 const minutesInput = document.querySelector('#number');
 const startButton = document.querySelector('#start');
 const timerDisplay = document.querySelector('#minutes');
-const set = document.querySelector('#set');
+const setButton = document.querySelector('#set');
 let countdownInterval;
-set.addEventListener('click', () => {
+function updateTimerDisplay(timeInSeconds) {
+
+  const hours = Math.floor(timeInSeconds / 3600);
+  const minutes = Math.floor((timeInSeconds % 3600)/ 60) ;
+  const seconds = timeInSeconds % 60;
+timerDisplay.textContent = `${hours.toString().padStart(2,'0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+};
+
+setButton.addEventListener('click', () => {
   let timeInMinutes = parseInt(minutesInput.value);
   if (isNaN(timeInMinutes) || timeInMinutes <= 0) {
     alert('Please enter a valid number of minutes.');
     return;
   }
-    let timeInSeconds = timeInMinutes * 60;
-      const hours = Math.floor(timeInSeconds / 3600);
-      const minutes = Math.floor(timeInSeconds / 60) % 60;
-      const seconds = timeInSeconds % 60;
-    timerDisplay.textContent = `${hours.toString().padStart(2,'0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
-  });
+  let timeInSeconds = timeInMinutes * 60;
+  updateTimerDisplay(timeInSeconds);
+})
 
 startButton.addEventListener('click', () => {
   let timeInMinutes = parseInt(minutesInput.value);
@@ -22,19 +27,20 @@ startButton.addEventListener('click', () => {
     alert('Please enter a valid number of minutes.');
     return;
   }
+
   let timeInSeconds = timeInMinutes * 60;
+  
   clearInterval(countdownInterval);
-    countdownInterval = setInterval(() => {
-      const hours = Math.floor(timeInSeconds / 3600);
-      const minutes = Math.floor(timeInSeconds/60) %60;
-      const seconds = timeInSeconds % 60;
-    timerDisplay.textContent = `${hours.toString().padStart(2,'0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
-    timeInSeconds--;
-    if (timeInSeconds < 0) {
-      clearInterval(countdownInterval);
-      timerDisplay.textContent = '00:00:00';
-      alert('Time is up!');
-    }
-  }, 1000); 
-   
-});
+  
+  countdownInterval = setInterval(() => {
+    
+      updateTimerDisplay(timeInSeconds);
+      timeInSeconds--;
+      if (timeInSeconds < 0) {
+        clearInterval(countdownInterval);
+        timerDisplay.textContent = '00:00:00';
+        alert('Time is up!');
+      }
+
+      }, 1000); 
+    });
