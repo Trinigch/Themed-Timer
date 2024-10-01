62a13a3e8ea5120d480d8ed17f87d7c415534506
modifiedyoke <bshurts@outlook.com>
2024-10-01 10:04:05 -0500

    Uploading work

---
+++ script.js
@@ -0,0 +1,26 @@
+const themeButton1 = document.querySelector('#theme1');
+const themeButton2 = document.querySelector('#theme2');
+const themeButton3 = document.querySelector('#theme3');
+const themeButton4 = document.querySelector('#theme4');
+
+let currentTheme = "none";
+
+themeButton1.addEventListener('click', function () {
+    alert('button clicked');
+}
+)
+
+themeButton2.addEventListener('click', function () {
+    alert('button clicked');
+}
+)
+
+themeButton3.addEventListener('click', function () {
+    alert('button clicked');
+}
+)
+
+themeButton4.addEventListener('click', function () {
+    alert('button clicked');
+}
+)
